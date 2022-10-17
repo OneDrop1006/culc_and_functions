9961369b731e24c0db72e57c7c626c48867041fd
OneDrop1006 <62099536+OneDrop1006@users.noreply.github.com>
2022-10-18 07:33:09 +0900

    del functions.py add words.py
    del functions and put functions into new file "words.py" as class method

---
+++ words.py
@@ -0,0 +1,21 @@
+# #words info object
+class words:
+    word = ""
+    meadning = ""
+
+    # reg words
+    def reg_words():
+        return
+
+    # edit words
+    def edit_words():
+        return
+
+    # del words
+    def del_words():
+        return
+
+    # #for common
+    # list del_words
+    def show_words():
+        return
